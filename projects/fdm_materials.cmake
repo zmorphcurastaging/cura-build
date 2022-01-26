a93196dd42d4d70c29e38b24d19cd0b432222dd7
sqsoft-devops <l.rosa@soniqsoft.pl>
2022-01-26 11:06:24 +0000

    change github repos to zmorph

--- projects/fdm_materials.cmake
+++ projects/fdm_materials.cmake
@@ -1,5 +1,5 @@
 ExternalProject_Add(fdm_materials
-    GIT_REPOSITORY https://github.com/ultimaker/fdm_materials
+    GIT_REPOSITORY https://github.com/zmorph/fdm_materials
     GIT_TAG origin/${FDMMATERIALS_BRANCH_OR_TAG}
     GIT_SHALLOW 1
     STEP_TARGETS update
