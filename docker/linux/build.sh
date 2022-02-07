f3f9883bc2e587f9eeffc0d127b706f9f0b7d996
e1479abfe2 <urosa9@gmail.com>
2022-02-07 10:39:55 +0100

    Update build.sh

--- docker/linux/build.sh
+++ docker/linux/build.sh
@@ -77,6 +77,7 @@ docker run \
   ${DOCKER_EXTRA_ARGS} \
   --user $(id -u):$(id -g) \
   --volume "$(pwd)":/home/ultimaker/src \
+  --volume /root/docker_run:/root \
   --name cura-build-environment \
   --env CURA_BUILD_OUTPUT_DIR=/home/ultimaker/src/output \
   --env CURA_BRANCH_OR_TAG="${CURA_BRANCH_OR_TAG}" \
