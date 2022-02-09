053c5aadaf177bb815c596536e61567fdd3dc4d5
e1479abfe2 <urosa9@gmail.com>
2022-02-09 09:25:22 +0100

    Update sha1sum_gen.sh

--- signing/sha1sum_gen.sh
+++ signing/sha1sum_gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pwd
-#sha1sum Ultimaker_Cura-0.0.0.AppImage > Ultimaker_Cura-0.0.0.AppImage.sha1
+cd output/appimages
+sha1sum Ultimaker_Cura-0.0.0.AppImage > Ultimaker_Cura-0.0.0.AppImage.sha1
