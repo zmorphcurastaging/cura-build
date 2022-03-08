7252520b2750b82a33a02eebbab79ae173b44d0d
e1479abfe2 <urosa9@gmail.com>
2022-03-08 14:34:51 +0100

    Update windows.cmake

--- signing/windows.cmake
+++ signing/windows.cmake
@@ -5,19 +5,21 @@
 
 find_package(Signtool REQUIRED)
 
-set(WINDOWS_IDENTITIY_PFX_FILE "THE_PFX_FILE_IS_MISSING_HERE!" CACHE STRING "PFX file, which represents the identity of the developer.")
-set(WINDOWS_IDENTITIY_PFX_PASSWORD "" CACHE STRING "Password, which unlocks the PFX file (optional)")
+#set(WINDOWS_IDENTITIY_PFX_FILE "THE_PFX_FILE_IS_MISSING_HERE!" CACHE STRING "PFX file, which represents the identity of the developer.")
+set(WINDOWS_IDENTITIY_PFX_PASSWORD "" CACHE STRING "")
 
 set(signtool_OPTIONS /fd SHA256 /a /f ${WINDOWS_IDENTITIY_PFX_FILE})
 
-if(NOT ${WINDOWS_IDENTITIY_PFX_PASSWORD} EQUAL "")
-    set(signtool_OPTIONS ${signtool_OPTIONS} /p ${WINDOWS_IDENTITIY_PFX_PASSWORD})
-else()
-    message(FATAL_ERROR "You can't sign your executables without passing your password here!") # Sadly...
-endif()
+#if(NOT ${WINDOWS_IDENTITIY_PFX_PASSWORD} EQUAL "")
+#    set(signtool_OPTIONS ${signtool_OPTIONS} /p ${WINDOWS_IDENTITIY_PFX_PASSWORD})
+#else()
+#    message(FATAL_ERROR "You can't sign your executables without passing your password here!") # Sadly...
+#endif()
+
 
 if(EXISTS ${WINDOWS_IDENTITIY_PFX_FILE})
     message(STATUS "Signing executables with: " ${WINDOWS_IDENTITIY_PFX_FILE})
+    message( ${CPACK_PACKAGE_FILE_NAME} )
     if(${WINDOWS_IDENTITIY_PFX_PASSWORD})
         message(WARNING "USE WITH CAUTION: Password for the PFX file has been set!")
     endif()
