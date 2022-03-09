be435ab07b5d6f1cd45328e11b7941f3d3f53a11
e1479abfe2 <urosa9@gmail.com>
2022-03-09 11:25:07 +0100

    Update windows_installer_exe_signing.ps1

--- signing/windows_installer_exe_signing.ps1
+++ signing/windows_installer_exe_signing.ps1
@@ -1,2 +1,2 @@
 param ($certificate)
-'C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe' sign  /fd SHA256 /a /f $certificate ./windows-installers/output/Ultimaker_Cura*.exe
+signtool sign  /fd SHA256 /a /f $certificate ./windows-installers/output/Ultimaker_Cura*.exe
