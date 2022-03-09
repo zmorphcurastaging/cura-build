13ee99c608c345882462b0492b9ad7abe32c911d
e1479abfe2 <urosa9@gmail.com>
2022-03-09 13:08:20 +0100

    signing

--- signing/windows_installer_exe_signing.ps1
+++ signing/windows_installer_exe_signing.ps1
@@ -1,2 +1,3 @@
-param ($certificate)
-signtool sign  /fd SHA256 /a /f $certificate ./windows-installers/output/Ultimaker_Cura*.exe
+$file_name = @(Get-ChildItem ./windows-installers/output/* -include *.exe)
+$file = $file_name[0]
+certutil -hashfile $file sha1 | Select -First 2 | Out-File ./windows-installers/output/$file.sha1
