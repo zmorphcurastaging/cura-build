050f8dedfa78fa5b4565cbc98372f09bb104db08
e1479abfe2 <urosa9@gmail.com>
2022-03-16 08:46:18 +0100

    Update windows_installer_exe_signing.ps1

--- signing/windows_installer_exe_signing.ps1
+++ signing/windows_installer_exe_signing.ps1
@@ -1,11 +1,12 @@
 # Get file name 
-$file_name = @(Get-ChildItem ./windows-installers/output/* -include *.exe)
 $path = "./windows-installers/output/"
-# extract file name from $file_name with .exe extension
-$exe_file_name =  Split-Path $file_name -leaf
+$full_file_name = @(Get-ChildItem $path* -include *.exe, *.msi)
+$file_name =  Split-Path $full_file_name -leaf
+$extension = $file_name -split '\.' | select -Last 1
+
 # change extension from .exe to .sha1 to save file
-$sha_file_name = $exe_file_name -replace '.exe', '_verify.sha1'
+$sha_file_name = $file_name -replace $extension, '_verify.sha1'
 # change directory to file folder, generate sha1sum and change directory back to other scripts work properly
 cd $path
-certutil -hashfile $exe_file_name sha1 | Select -First 2 | Out-File $sha_file_name
+certutil -hashfile $file_name sha1 | Select -First 2 | Out-File $sha_file_name
 cd ../../
