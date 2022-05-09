8c38b5f1e19e532c64d9da7a606ed2e7c7731688
sqsoft-devops <l.rosa@soniqsoft.pl>
2022-05-09 13:58:11 +0200

    Update build.ps1

--- docker/windows/build.ps1
+++ docker/windows/build.ps1
@@ -1,10 +1,10 @@
 # This script builds a Cura release using the cura-build-environment Windows docker image.
 $branch=$args[0]
 write-host $branch
-$branch_array =$branch.Split(".")
+$branch_array = $branch.Split(".")
 write-host $branch_array
-$fisrt_number= $branch_array[0]
-$second_number= $branch_array[1]
+$fisrt_number = $branch_array[0]
+$second_number = $branch_array[1]
 
 write-host "first number"
 write-host $fisrt_number
