862abbbf7771a3decc1f980b820f350e1cebbf11
sqsoft-devops <l.rosa@soniqsoft.pl>
2022-05-17 12:40:16 +0200

    change names to Zmorph Divid

--- signing/fix_codesign_issues.sh
+++ signing/fix_codesign_issues.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd "Ultimaker Cura.app/Contents/MacOS"
+pushd "Zmorph Divid.app/Contents/MacOS"
 
 dirs=`find . -type d -depth 1`
 
--- signing/sign_cura_macOS.sh
+++ signing/sign_cura_macOS.sh
@@ -2,13 +2,13 @@
 
 # Sign the libraries of the application
 echo "===================================="
-echo "Signing the libraries of Cura . . ."
+echo "Signing the libraries of Divid . . ."
 echo "===================================="
-find ./Ultimaker\ Cura.app -name "*.dylib" -exec codesign --verbose --force --sign $codesign_identity {} \;
-find ./Ultimaker\ Cura.app -name "*.so" -exec codesign --entitlements ../../signing/cura.entitlements --verbose --force -o runtime --sign $codesign_identity {} \;
-
+find ./Ultimaker\ Divid.app -name "*.dylib" -exec codesign --verbose --force --sign $codesign_identity {} \;
+find ./Ultimaker\ Divid.app -name "*.so" -exec codesign --entitlements ../../signing/cura.entitlements --verbose --force -o runtime --sign $codesign_identity {} \;
+Divid
 # Sign the application itself
 echo "===================================="
-echo "Signing the Ultimaker Cura.app . . ."
+echo "Signing the Zmorph Divid.app . . ."
 echo "===================================="
-codesign --entitlements ../../signing/cura.entitlements --verbose --force --deep -o runtime --sign $codesign_identity "Ultimaker Cura.app"
+codesign --entitlements ../../signing/cura.entitlements --verbose --force --deep -o runtime --sign $codesign_identity "Zmorph Divid.app"
